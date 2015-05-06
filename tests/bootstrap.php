10a91b4565900254fb73f0304576f1457dc45380
Carsten Brandt <mail@cebe.cc>
2015-05-06 16:57:28 +0200

    fixed test break

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,12 +15,12 @@ Yii::setAlias('@yiiunit/extensions/elasticsearch', __DIR__);
 Yii::setAlias('@yii/elasticsearch', dirname(__DIR__));
 
 if (!class_exists('\yiiunit\framework\ar\ActiveRecordTestTrait')) {
-    if (is_file(__DIR__ . '/../../../tests/unit/framework/ar/ActiveRecordTestTrait.php')) {
-        require_once(__DIR__ . '/../../../tests/unit/framework/ar/ActiveRecordTestTrait.php');
+    if (is_file(__DIR__ . '/../../../tests/framework/ar/ActiveRecordTestTrait.php')) {
+        require_once(__DIR__ . '/../../../tests/framework/ar/ActiveRecordTestTrait.php');
     } elseif (is_file(__DIR__ . '/ActiveRecordTestTrait.php')) {
         require_once(__DIR__ . '/ActiveRecordTestTrait.php');
     } else {
-        file_put_contents(__DIR__ . '/ActiveRecordTestTrait.php', file_get_contents('https://raw.githubusercontent.com/yiisoft/yii2/master/tests/unit/framework/ar/ActiveRecordTestTrait.php'));
+        file_put_contents(__DIR__ . '/ActiveRecordTestTrait.php', file_get_contents('https://raw.githubusercontent.com/yiisoft/yii2/master/tests/framework/ar/ActiveRecordTestTrait.php'));
         require_once(__DIR__ . '/ActiveRecordTestTrait.php');
     }
 }
