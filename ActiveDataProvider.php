41dfe142bb92ecbe137ebe8c6ca53f12f1aba315
Klimov Paul <klimov.paul@gmail.com>
2015-06-17 17:12:30 +0300

    Aggregation access shortcut methods added to `ActiveDataProvider`

--- ActiveDataProvider.php
+++ ActiveDataProvider.php
@@ -7,13 +7,19 @@
 
 namespace yii\elasticsearch;
 
+use yii\base\InvalidCallException;
 use yii\base\InvalidConfigException;
 
 /**
  * ActiveDataProvider is an enhanced version of [[\yii\data\ActiveDataProvider]] specific to the ElasticSearch.
  * It allows to fetch not only rows and total rows count, but full query results including aggregations and so on.
  *
+ * Note: this data provider fetches result models and total count using single ElasticSearch query, so results total
+ * count will be fetched after pagination limit applying, which eliminates ability to verify if requested page number
+ * actually exist. Data provider disables `yii\data\Pagination::validatePage` automatically because of this.
+ *
  * @property array $queryResults the query results.
+ * @property array $aggregations all aggregations results.
  *
  * @author Paul Klimov <klimov.paul@gmail.com>
  * @since 2.0.5
@@ -43,6 +49,30 @@ class ActiveDataProvider extends \yii\data\ActiveDataProvider
             $this->prepare();
         }
         return $this->_queryResults;
+    }
+
+    /**
+     * @return array all aggregations results
+     */
+    public function getAggregations()
+    {
+        $results = $this->getQueryResults();
+        return isset($results['aggregations']) ? $results['aggregations'] : [];
+    }
+
+    /**
+     * Returns results of the specified aggregation.
+     * @param string $name aggregation name.
+     * @return array aggregation results.
+     * @throws InvalidCallException if requested aggregation does not present in query results.
+     */
+    public function getAggregation($name)
+    {
+        $aggregations = $this->getAggregations();
+        if (!isset($aggregations[$name])) {
+            throw new InvalidCallException("Aggregation '{$name}' does not present.");
+        }
+        return $aggregations[$name];
     }
 
     /**
